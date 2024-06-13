7d8325aec0a9019ba4c7600cb0950ba7b0d9901e
paulosvdd05 <paulosvdd05@gmail.com>
2024-06-13 07:17:42 -0300

    Adicionando common.scss

--- src/components/Respostas.js
+++ src/components/Respostas.js
@@ -1,23 +1,36 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import { animacaoClick } from '../scripts/animations.js';
 
 const initialState = {
     respostas: [
-        {id:0, resposta: "São Paulo", correta: false, selecionada:false},
-        {id:1, resposta: "Rio de Janeiro", correta: false, selecionada:false},
-        {id:2, resposta: "Brasília", correta: true, selecionada:false},
-        {id:3, resposta: "Curitiba", correta: false, selecionada:false},
+        { id: 0, resposta: "São Paulo", correta: false, selecionada: false },
+        { id: 1, resposta: "Rio de Janeiro", correta: false, selecionada: false },
+        { id: 2, resposta: "Brasília", correta: true, selecionada: false },
+        { id: 3, resposta: "Curitiba", correta: false, selecionada: false },
     ]
 }
 
 export default class Respostas extends Component {
 
-    state = {...initialState}
+    state = { ...initialState }
+
+    animacaoClick = (id) => {
+        this.setState(prevState => ({
+            respostas: prevState.respostas.map(
+                resposta => resposta.id === id ? { ...resposta, selecionada: true } : { ...resposta, selecionada: false }
+            )
+        }));
+    }
+ 
+
+    classeResumo = (resposta) => {
+    
+    }
 
     renderizarRespostas = () => {
         return this.state.respostas.map((resposta, index) => {
             return (
-                <div key={index} className="container container-resposta" id={`resposta${resposta.id}`}>
+                <div onClick={() => this.animacaoClick(resposta.id)} key={index} className={`container container-resposta ${resposta.selecionada ? 'animacao-click clicked' : ''}`} id={`resposta${resposta.id}`}>
                     <h2>{resposta.resposta}</h2>
                 </div>
             )
@@ -28,7 +41,7 @@ export default class Respostas extends Component {
         return (
             <div>
                 {this.renderizarRespostas()}
-        
+
             </div>
         )
     }
